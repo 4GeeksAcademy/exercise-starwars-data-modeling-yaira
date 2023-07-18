5383b264ccafc569f1f6b3d5636aa2cd728aca47
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-07-18 00:13:07 +0000

    1

--- src/models.py
+++ src/models.py
@@ -7,23 +7,30 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+
+
+
+class UserFavorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    favorited = Column(String(25))
+    itsUser = relationship(User)
+    user_id = Column(Integer, ForeignKey('User.id'))
+
+class Character(Base):
+    __tablename__ = 'Character'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Planet_Name = Column(String(250))
+    Character = relationship(Character)
 
     def to_dict(self):
         return {}
